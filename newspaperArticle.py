38cd41f1fdc175ced2085ec26c5237e06d7cfd43
Ajinkya <albus702@gmail.com>
2017-06-16 17:46:03 +0530

    K-Nearest Neighbour Theory

---
+++ newspaperArticle.py
@@ -0,0 +1,19 @@
+############################################################################################################################################################
+# Supervised Learning
+
+# Newspaper Article Classifier (K-Nearest Neighbour Classifier)
+
+# 1. Create a corpus of news articles which are already classified into Tech & Non-Tech.
+#     a. Download Tech news articles and label them as Tech.
+#     b. Download Non-Tech news articles and label them as Non-Tech.
+# 2. Create a problem instance from a blog. An article that needs to be classified.
+# 3. Use the KNN Algorithm to classify the problem instance.
+#     a. Represent each article as a vector of the 25 most important words in the article.
+#     b. The distance between articles is calculated using the number of important words that they have in common.
+#     c. Find the K-Nearest Neighbours to carry out a majority vote. Assign the article to that category.
+
+
+
+############################################################################################################################################################
+
+
