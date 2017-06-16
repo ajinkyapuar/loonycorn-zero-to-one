6b74159ba35f4962bdc0635d547cc73185795d3f
Ajinkya <albus702@gmail.com>
2017-06-16 18:06:51 +0530

    Sentiment Analysis Theory

---
+++ sentimentAnalysis.py
@@ -0,0 +1,14 @@
+############################################################################################################################################################
+# Supervised Learning
+
+# TODO: Sentiment Analysis
+
+# 1. Create a corpus of texts which are already classified into Positive & Negative.
+# 2. Create a problem instance. An text that needs to be classified.
+# 3. Use the NB / KNN / SVM Algorithm to classify the problem instance.
+
+
+
+############################################################################################################################################################
+
+
